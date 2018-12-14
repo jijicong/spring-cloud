893a114ee8b65c33f333a4877e72a758236b3f77
hzcyn <hzcyn@tairanchina.com>
2018-12-14 14:31:06 +0800

    Merge branch 'master' of rep.360taihe.com:scm-group/scm-purchase-cloud
    # Conflicts:
    #	.idea/codeStyles/Project.xml

--- scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
@@ -3,9 +3,11 @@ package org.trc.scm.purchase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class ScmPurchaseApplication {
 
 	public static void main(String[] args) {
---
+++ scm-purchase/src/main/java/org/trc/scm/purchase/controller/PurchaseController.java
@@ -0,0 +1,10 @@
+package org.trc.scm.purchase.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hzcyn on 2018/12/14.
+ */
+@RestController
+public class PurchaseController {
+}
