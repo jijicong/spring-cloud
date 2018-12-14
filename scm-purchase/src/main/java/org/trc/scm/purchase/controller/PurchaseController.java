53d3ff12da7ce9791f7e931c4e39cd265a9cdb66
hzcyn <hzcyn@tairanchina.com>
2018-12-14 17:49:15 +0800

    修改配置

--- scm-purchase/src/main/java/org/trc/scm/purchase/controller/PurchaseController.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/controller/PurchaseController.java
@@ -1,5 +1,7 @@
 package org.trc.scm.purchase.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,4 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PurchaseController {
+
+    @Value("${name.chenyn}")
+    private String name;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.name;
+    }
 }
