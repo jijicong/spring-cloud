8aa6fb33b8fc9b08f2da8adf7416eafdf9f02cd5
hzluoxc <hzluoxc@tairanchina.com>
2018-12-18 11:52:22 +0800

    demo测试编写

---
+++ scm-provider/scm-provider-finance/src/main/java/org/trc/scm/provider/finance/remote/FinanceRemote.java
@@ -0,0 +1,14 @@
+package org.trc.scm.provider.finance.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+/**
+ * @Auther: hzluoxingcheng
+ * @Date: 2018/12/18 11:51
+ * @Description: feign
+ */
+@FeignClient
+public interface FinanceRemote {
+
+
+}
