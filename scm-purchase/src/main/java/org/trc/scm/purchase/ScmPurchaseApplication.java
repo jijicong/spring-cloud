fbf371d16cf8e19d5f5a878b594dcd24b9931357
吴东雄 <hzwdx@tairanchina.com>
2018-12-18 19:08:41 +0800

    修改配置服务的git 接入rabbitmq通知更新远程git更新

--- scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
@@ -1,7 +1,9 @@
 package org.trc.scm.purchase;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "org.trc.scm.provider")
 @ComponentScan(basePackages = {"org.trc.scm.purchase","org.trc.scm.provider"})
 @tk.mybatis.spring.annotation.MapperScan(basePackages = "org.trc.scm.purchase.dao")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ScmPurchaseApplication {
 
 	public static void main(String[] args) {
--- scm-purchase/src/main/java/org/trc/scm/purchase/controller/api/PurchaseClient.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/controller/api/PurchaseClient.java
@@ -2,7 +2,7 @@ package org.trc.scm.purchase.controller.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.trc.scm.provider.purchase.remote.PurchaseRemote;
 
@@ -13,11 +13,23 @@ import org.trc.scm.provider.purchase.remote.PurchaseRemote;
 @RestController
 public class PurchaseClient implements PurchaseRemote {
 
-    @Value("${mybatis.type-aliases-package}")
-    private String mybatis;
+    /*@Value("${mybatis.type-aliases-package}")
+    private String mybatis;*/
+    @Value("${foo}")
+    private String foo;
 
-    @Override
+    /*@Override
     public String hello(@RequestParam(value = "name") String name) {
         return name + mybatis;
+    }*/
+
+    @RequestMapping(value = "/test")
+    public String test(){
+        return foo;
+    }
+
+    @Override
+    public String hello(String name) {
+        return null;
     }
 }
