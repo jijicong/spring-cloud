3602c3a27e4176b555c24b3db14fcd8f52284473
吴东雄 <hzwdx@tairanchina.com>
2018-12-19 14:28:18 +0800

    测试配置修改

--- scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
@@ -1,16 +1,15 @@
 package org.trc.scm.purchase;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@RefreshScope
 @tk.mybatis.spring.annotation.MapperScan(basePackages = "org.trc.scm.purchase.dao")
 public class ScmPurchaseApplication {
 
