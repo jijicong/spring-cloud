76b01cc32cd9e46e679fc7ff9b7f57d1aad40305
hzluoxc <hzluoxc@tairanchina.com>
2018-12-20 17:58:18 +0800

    修改

--- scm-product/src/main/java/org/trc/scm/product/biz/impl/ProductorServiceImpl.java
+++ scm-product/src/main/java/org/trc/scm/product/biz/impl/ProductorServiceImpl.java
@@ -6,6 +6,7 @@ import org.trc.scm.common.support.BaseService;
 import org.trc.scm.product.core.ProductCore;
 import org.trc.scm.product.model.domain.Items;
 import org.trc.scm.product.biz.ProductorService;
+import org.trc.scm.product.remote.PurchaseRemote;
 
 /**
  * @Auther: hzluoxingcheng
@@ -19,6 +20,9 @@ public class ProductorServiceImpl implements ProductorService {
     @Autowired
     private ProductCore productCore;
 
+    @Autowired
+    private PurchaseRemote purchaseRemote;
+
     /**
      * @Description: 根据spucode查询商品记录
      * @Author: hzluoxingcheng
@@ -29,6 +33,10 @@ public class ProductorServiceImpl implements ProductorService {
 //        Items items = new Items();
 //        items.setSpuCode(spuCode);
 //        return this.selectOne(items);
+
+        System.out.println("开始远程调用");
+        purchaseRemote.test();
+        System.out.println("结束远程调用");
 
         //通过公共层调用
         return productCore.getItemBySpuCode(spuCode);
--- scm-product/src/main/java/org/trc/scm/product/controller/api/ProductApi.java
+++ scm-product/src/main/java/org/trc/scm/product/controller/api/ProductApi.java
@@ -3,9 +3,8 @@ package org.trc.scm.product.controller.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.trc.scm.product.model.domain.Items;
-import org.trc.scm.product.remote.PurchaseRemote;
 import org.trc.scm.product.biz.ProductorService;
+import org.trc.scm.product.model.domain.Items;
 
 
 /**
@@ -20,8 +19,6 @@ public class ProductApi {
     @Autowired
     private ProductorService productorService;
 
-    @Autowired
-    private PurchaseRemote purchaseRemote;
 
     /**
      * @Description: 根据spucode查询商品记录
@@ -30,9 +27,6 @@ public class ProductApi {
      */ 
     @GetMapping("/getItemBySpuCode")
     public Items getItemBySpuCode(String spuCode){
-        System.out.println("开始远程调用");
-        purchaseRemote.test();
-        System.out.println("结束远程调用");
        return  productorService.getItemBySpuCode(spuCode);
     }
 
