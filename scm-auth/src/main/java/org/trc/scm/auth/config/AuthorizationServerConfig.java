95cf393602613e8ae5cb35cb149da63d7e9649c8
hzcyn <hzcyn@tairanchina.com>
2018-12-21 14:25:11 +0800

    auth

--- scm-auth/src/main/java/org/trc/scm/auth/config/AuthorizationServerConfig.java
+++ scm-auth/src/main/java/org/trc/scm/auth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package org.trc.scm.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
--- scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
+++ scm-purchase/src/main/java/org/trc/scm/purchase/ScmPurchaseApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-//@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
